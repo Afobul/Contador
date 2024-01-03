1cd999ae4f9ba67d4e3dedf6759832b3fc5dfc49
Afobul <128596562+Afobul@users.noreply.github.com>
2024-01-03 09:24:21 -0100

    Add files via upload

---
+++ contador.sh
@@ -0,0 +1,46 @@
+# #!/bin/bash
+
+# contador=1
+
+
+#     echo "Nº1:"
+#     for contador in {1..5}
+#     do
+#         echo $contador
+#     done
+
+#     echo "Nº2:"
+#     for contador in {1..5}
+#     do
+#         echo $contador
+#     done
+
+#     echo "Nº3:"
+#     for contador in {1..5}
+#     do
+#         echo $contador
+#     done
+
+#     echo "Nº4:"
+#     for contador in {1..5}
+#     do
+#         echo $contador
+#     done
+
+#     echo "Nº5:"
+#     for contador in {1..5}
+#     do
+#         echo $contador
+#     done
+
+# #!/bin/bash
+
+# for i in {1..5}; do
+#     echo -n "Nº$i -> "
+#     numeros=""
+#     for contador in {1..5}; do
+#         numeros+="$contador, "
+#     done
+#     echo ${numeros%, }  # Remove a vírgula e o espaço extras no final antes de imprimir
+# done
+
